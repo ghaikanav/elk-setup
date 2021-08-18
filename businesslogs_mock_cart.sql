dd92bc009800f96d2453279fe7a88edf450603bd
Kanav Ghai <kanghai@publicisgroupe.net>
2021-08-18 12:58:58 +0530

    added AC2

---
+++ businesslogs_mock_cart.sql
@@ -0,0 +1,6 @@
+INSERT INTO businesslogs_mock (id,message,log_date) VALUES (11,'transaction successfully completed with cart size:5','2021-08-21');
+INSERT INTO businesslogs_mock (id,message,log_date) VALUES (12,'transaction successfully completed with cart size:6','2021-08-22');
+INSERT INTO businesslogs_mock (id,message,log_date) VALUES (13,'transaction successfully completed with cart size:11','2021-08-23');
+INSERT INTO businesslogs_mock (id,message,log_date) VALUES (14,'transaction successfully completed with cart size:16','2021-08-24');
+INSERT INTO businesslogs_mock (id,message,log_date) VALUES (15,'transaction successfully completed with cart size:4','2021-08-24');
+INSERT INTO businesslogs_mock (id,message,log_date) VALUES (16,'transaction successfully completed with cart size:7','2021-08-24');
